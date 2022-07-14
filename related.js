fa9a5ae808820a672c0fc925397bf1c6a4e85bf6
Gurtaj <gurtajs98@gmail.com>
2022-07-14 14:19:13 +0100

    Revised version of rps

--- related.js
+++ related.js
@@ -17,6 +17,7 @@ function computerPlay() {
    return move[Math.floor(Math.random()*move.length)]
 }
 
+
 function playRound(playerSelection, computerSelection) {
    if ((playerSelection == "Rock" && computerSelection == "Paper")) {
       return `You lose, ${computerSelection} beat ${playerSelection}. Computer gains ${++computerCount} point`;
@@ -42,24 +43,65 @@ function playRound(playerSelection, computerSelection) {
       ++playerCount;
       return "You win, scissors beat paper";    
    }
-   }
    
-                              
+   }
 
-function game() {
-   for (let i=0; i < 5; i++) {
-      const computerSelection = computerPlay();
-      let input = prompt("Choose Rock, Paper or scissors?");
-      let playerSelection = input.substring(0,1).toUpperCase() + input.substring(1).toLowerCase();
-      console.log(playRound(playerSelection, computerSelection));
-   }
-   if (playerCount > computerCount) {
-      return "You win the game"
-   }
-   else {
-      return "You lose! computer wins the game"
-   }
-}
-console.log(game());
 
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach((button) => {
+   button.addEventListener('click', () => {
+   const playerSelection = button.className;
+   const computerSelection = computerPlay();
+
+   pElement.textContent = (playRound(playerSelection, computerSelection));
+   container.appendChild(pElement);
+
+   score.textContent = "Player score: " + playerCount + " Computer score: " + computerCount;
+   container.appendChild(score);
+
+   endgame();
+ 
+})});
+
+const container = document.querySelector('#container');
+const pElement = document.createElement('p');
+
+const score = document.createElement('p');
+
+const winnerPl = document.createElement('p');
+winnerPl.style.color = "blue";
+
+const winnerComp = document.createElement('p');
+winnerComp.style.color = "red"
+
+const refresh = document.createElement('button');
+
+function endgame() {
+   if (playerCount == 5) {
+      winnerPl.textContent = "You win the game!";
+      container.appendChild(winnerPl);
+      document.querySelector(".Rock").disabled = true;
+      document.querySelector(".Paper").disabled = true;
+      document.querySelector(".Scissors").disabled = true;
+      
+      refresh.textContent = "New Game";
+      container.appendChild(refresh);
+   }
+   else if (computerCount == 5) {
+      winnerComp.textContent = "You lose, computer wins!";
+      container.appendChild(winnerComp);
+      document.querySelector(".Rock").disabled = true;
+      document.querySelector(".Paper").disabled = true;
+      document.querySelector(".Scissors").disabled = true;
+      refresh.textContent = "New Game";
+      container.appendChild(refresh);
+   }
+
+   }
+
+refresh.addEventListener('click', () => {
+   window.location.reload();
+})
 
